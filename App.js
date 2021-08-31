23f7098381cfb73821fbe52f4dbce7b7491d2402
Wolkentreiber <ishevchuk1998@gmail.com>
2021-08-31 15:20:25 +0300

    added encapsulated text style

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, Image, SafeAreaView, View, Dimensions } from 'react-native';
+
 import VeiwImageScreen from './app/screens/VeiwImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import AppText from './app/components/AppText';
 
 export default function App() {
 
   console.log(Dimensions.get("screen"))
   return (
-    <WelcomeScreen />
-    // <VeiwImageScreen />
+    <SafeAreaView>
+   <AppText>I loveReact Native!</AppText>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
